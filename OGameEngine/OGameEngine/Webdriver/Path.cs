e39964e813bf90ea10307c640181611b683fb063
Gabriel Calmeiro <gabriel.calmeiro@farfetch.com>
2021-10-13 13:20:15 +0100

    Fix xpath of research lvl when lvling up

--- OGameEngine/OGameEngine/Webdriver/Path.cs
+++ OGameEngine/OGameEngine/Webdriver/Path.cs
@@ -13,22 +13,22 @@
 
         public class Researches
         {
-            public const string EnergyLevel = "//*[@id=\"technologies_basic\"]/ul/li[1]/span/span[1]/span[1]";
-            public const string LaserLevel = "//*[@id=\"technologies_basic\"]/ul/li[2]/span/span[1]/span[1]";
-            public const string IonLevel = "//*[@id=\"technologies_basic\"]/ul/li[3]/span/span[1]/span[1]";
-            public const string HyperspaceLevel = "//*[@id=\"technologies_basic\"]/ul/li[4]/span/span[1]/span[1]";
-            public const string PlasmaLevel = "//*[@id=\"technologies_basic\"]/ul/li[5]/span/span[1]/span[1]";
-            public const string CombustionLevel = "//*[@id=\"technologies_drive\"]/ul/li[1]/span/span[1]/span[1]";
-            public const string ImpulsionLevel = "//*[@id=\"technologies_drive\"]/ul/li[2]/span/span[1]/span[1]";
-            public const string HyperspacePropulsionLevel = "//*[@id=\"technologies_drive\"]/ul/li[3]/span/span[2]/span[1]";
-            public const string SpyLevel = "//*[@id=\"technologies_advanced\"]/ul/li[1]/span/span[1]/span[1]";
-            public const string ComputersLevel = "//*[@id=\"technologies_advanced\"]/ul/li[2]/span/span[1]/span[1]";
-            public const string AstrophysicsLevel = "//*[@id=\"technologies_advanced\"]/ul/li[3]/span/span[1]/span[1]";
-            public const string IntergalacticLevel = "//*[@id=\"technologies_advanced\"]/ul/li[4]/span/span[1]/span[1]";
-            public const string GravitationLevel = "//*[@id=\"technologies_advanced\"]/ul/li[5]/span/span[1]/span[1]";
-            public const string WeaponsLevel = "//*[@id=\"technologies_combat\"]/ul/li[1]/span/span[1]/span[1]";
-            public const string ShieldingLevel = "//*[@id=\"technologies_combat\"]/ul/li[2]/span/span[1]/span[1]";
-            public const string ArmorLevel = "//*[@id=\"technologies_combat\"]/ul/li[3]/span/span[1]/span[1]";
+            public const string EnergyLevel = "//*[@id=\"technologies_basic\"]/ul/li[1]/span/span[1]/span[1] | //*[@id=\"technologies_basic\"]/ul/li[1]/span/span[2]/span[1]";
+            public const string LaserLevel = "//*[@id=\"technologies_basic\"]/ul/li[2]/span/span[1]/span[1] | //*[@id=\"technologies_basic\"]/ul/li[2]/span/span[2]/span[1]";
+            public const string IonLevel = "//*[@id=\"technologies_basic\"]/ul/li[3]/span/span[1]/span[1] | //*[@id=\"technologies_basic\"]/ul/li[3]/span/span[2]/span[1]";
+            public const string HyperspaceLevel = "//*[@id=\"technologies_basic\"]/ul/li[4]/span/span[1]/span[1] | //*[@id=\"technologies_basic\"]/ul/li[4]/span/span[2]/span[1]";
+            public const string PlasmaLevel = "//*[@id=\"technologies_basic\"]/ul/li[5]/span/span[1]/span[1] | //*[@id=\"technologies_basic\"]/ul/li[5]/span/span[2]/span[1]";
+            public const string CombustionLevel = "//*[@id=\"technologies_drive\"]/ul/li[1]/span/span[1]/span[1] | //*[@id=\"technologies_drive\"]/ul/li[1]/span/span[2]/span[1]";
+            public const string ImpulsionLevel = "//*[@id=\"technologies_drive\"]/ul/li[2]/span/span[1]/span[1] | //*[@id=\"technologies_drive\"]/ul/li[2]/span/span[2]/span[1]";
+            public const string HyperspacePropulsionLevel = "//*[@id=\"technologies_drive\"]/ul/li[3]/span/span[1]/span[1] | //*[@id=\"technologies_drive\"]/ul/li[3]/span/span[2]/span[1]";
+            public const string SpyLevel = "//*[@id=\"technologies_advanced\"]/ul/li[1]/span/span[1]/span[1] | //*[@id=\"technologies_advanced\"]/ul/li[1]/span/span[2]/span[1]";
+            public const string ComputersLevel = "//*[@id=\"technologies_advanced\"]/ul/li[2]/span/span[1]/span[1] | //*[@id=\"technologies_advanced\"]/ul/li[2]/span/span[2]/span[1]";
+            public const string AstrophysicsLevel = "//*[@id=\"technologies_advanced\"]/ul/li[3]/span/span[1]/span[1] | //*[@id=\"technologies_advanced\"]/ul/li[3]/span/span[2]/span[1]";
+            public const string IntergalacticLevel = "//*[@id=\"technologies_advanced\"]/ul/li[4]/span/span[1]/span[1] | //*[@id=\"technologies_advanced\"]/ul/li[4]/span/span[2]/span[1]";
+            public const string GravitationLevel = "//*[@id=\"technologies_advanced\"]/ul/li[5]/span/span[1]/span[1] | //*[@id=\"technologies_advanced\"]/ul/li[5]/span/span[2]/span[1]";
+            public const string WeaponsLevel = "//*[@id=\"technologies_combat\"]/ul/li[1]/span/span[1]/span[1] | //*[@id=\"technologies_combat\"]/ul/li[1]/span/span[2]/span[1]";
+            public const string ShieldingLevel = "//*[@id=\"technologies_combat\"]/ul/li[2]/span/span[1]/span[1] | //*[@id=\"technologies_combat\"]/ul/li[2]/span/span[2]/span[1]";
+            public const string ArmorLevel = "//*[@id=\"technologies_combat\"]/ul/li[3]/span/span[1]/span[1] | //*[@id=\"technologies_combat\"]/ul/li[3]/span/span[2]/span[1]";
         }
     }
 }
